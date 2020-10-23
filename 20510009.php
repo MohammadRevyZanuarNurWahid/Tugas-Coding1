7413e1fb80a0fd84e841a2942efe6ceed98a0294
MohammadRevyZanuarNurWahid <72596646+MohammadRevyZanuarNurWahid@users.noreply.github.com>
2020-10-23 13:49:53 +0700

    Add files via upload

---
+++ 20510009.php
@@ -0,0 +1,22 @@
+<html>
+	<head><h1><center><b>Biodata Mohammad Revy Zanuar Nur Wahid</b></center></h1></head>
+	<body>
+
+		<br>Nama <b>:</b> Mohammad Revy Zanuar Nur Wahid</br>
+		<br>NIM <b>:</b> 20510009</br>
+		<br>Prodi <b>:</b> Sistem Informasi</br>
+		<br>Temapat/Tanggal Lahir <b>:</b> Brebes, 26 Januari 2002</br>
+		<br>Jenis Kelamin <b>:</b> Laki-laki</br>
+		<br>Alamat<b>:</b> Dsn.Wonosalam,Ds.Wonosalam,Kec.Wonosalam,Kab.Jombang,Jawa Timur</br>
+		<br>Agama<b>:</b> Islam</br>
+		<br>Nama Ayah<b>:</b>Wahid</br>
+		<br>Nama Ibu<b>:</b>Ririn Sukaryanigtyas</br>
+		<br>Status<b>:</b> Mahasiswa</br>
+		<br>No Telp<b>:</b> 085854159606</br>
+		<br>Kewerganegaraan <b>:</b> WNI</br>
+		<br>Hobi <b>:</b> Bermain Game</br>
+		<br>Motto<b>:</b> Janganlah Berhenti Berusaha Meskipun Hasil Tidak Sesuai</br>
+		<img src=C:/Program Files (x86)/xampp/htdocs/wer.JPEG" with=200 height=200>
+
+	</body>
+</html>
